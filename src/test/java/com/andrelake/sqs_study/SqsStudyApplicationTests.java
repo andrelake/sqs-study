a50d98c4a379fa1b3074e095db2a4a3efa609b60
andreLake <andre.lake@hotmail.com>
2025-04-18 12:58:13 -0300

    created

---
+++ src/test/java/com/andrelake/sqs_study/SqsStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrelake.sqs_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
